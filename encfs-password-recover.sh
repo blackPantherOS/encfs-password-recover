657da65ba25b70a42888abf3aaf0bed7b2a5e705
Charles Barcza <info@blackpanther.hu>
2017-12-23 14:59:52 +0100

    Added a coundown

--- encfs-password-recover.sh
+++ encfs-password-recover.sh
@@ -14,26 +14,47 @@
 ### encfs-password-recover.sh ~/ecripted ~/decripted /path/passwdlist
 ### enter the full path of folders and your password file!
 
+case $LANG in
+    hu*)
+	pass_num="Beolvasott jelszavak száma"
+	first="A"
+	key_txt="kulcs:"
+	result="eredménye"
+	found="Megtalált jelszó!"
+	recovered=" Kulcs helyreállítva - a jelszó:"
+    ;;
+    *)
+	pass_num="Number of readed passwds:"
+	first="The"
+	key_txt="key result:"
+	result="result"
+	found="Found password!"
+	recovered=" Key recovered - the password is:"
+    ;;
+esac
+
 [ ! -n "$1" ]&& echo "Missing first parameter: ~/encrypted directory" && exit
 [ ! -n "$2" ]&& echo "Missing second parameter: ~/Dencrypted directory" && exit
 [ ! -n "$3" ]&& echo "Missing 3rd parameter: ~/passwordlist file" && exit
 
 counter=1
 lines=$(expr `cat $3 | wc -l` + 1)
-echo "Passwds Num: $lines"
+echo "$pass_num: $lines"
+unlines=$lines
 sleep 1
 while [ true ]; do
-   echo -n "Kulcs: $(head -n $counter $3 | tail -n 1) -> " 
+   echo -n "$first ${unlines}. $key_txt $(head -n $counter $3 | tail -n 1) $result -> " 
    echo "$(head -n $counter $3 | tail -n 1)" | encfs $1 $2 --stdinpass 
     if [ "$?" -eq "0" ]; then
-	echo "Found password!"; echo
-	echo "  ****************************************************"
-        echo -n "  *  Key recovered - the password is: "
+	echo "$found"; echo
+	echo "  *********************************************************"
+        echo -n "  * $recovered "
         echo "$(head -n $counter $3 | tail -n 1)"
-	echo "  ****************************************************"
+	echo "  *********************************************************"
 	echo 
         exit
     fi
     counter=$(($counter + 1))
+    unlines=$(($unlines - 1))
     [ "$counter" = "$lines" ]&& exit 0
 done
